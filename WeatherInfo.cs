5ae133337911f33bb8c850ded1da21a1e5e5d952
technical-naveed <132186704+technical-naveed@users.noreply.github.com>
2023-04-30 14:31:05 +0500

    💙 զʊɛɛռ ɮʟʊɛ 💙 update by owner 📢

---
+++ WeatherInfo.cs
@@ -0,0 +1,43 @@
+using System;
+
+using Newtonsoft.Json;
+
+namespace BlueQueen
+
+{
+
+    public class WeatherInfo
+
+    {
+
+        [JsonProperty("ID")]
+
+        public int ID { get; set; }
+
+        [JsonProperty("Date")]
+
+        public DateTime Date { get; set; }
+
+        [JsonProperty("Value")]
+
+        public double Value { get; set; }
+
+    }
+
+	public class PressureInfo	{
+
+		[JsonProperty("ID")]
+
+		public int ID { get; set; }
+
+		[JsonProperty("Date")]
+
+		public DateTime Date { get; set; }
+
+		[JsonProperty("Value")]
+
+		public double Pressure { get; set; }
+
+	}
+
+}
